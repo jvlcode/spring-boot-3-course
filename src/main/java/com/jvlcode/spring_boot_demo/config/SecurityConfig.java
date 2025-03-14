648f0c386f113e2ca9b8f975777ccf8a02a3751b
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-03-14 12:01:44 +0530

    Create SecurityConfig
    1.Open the url  http://localhost:8080/api/users
    2.Data will be seen because of no security
    3.Create SecurityConfig to secure the url
    4.Open the url again and Now you cannot see the data
    5.Response will be 403 Forbidden

---
+++ src/main/java/com/jvlcode/spring_boot_demo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.jvlcode.spring_boot_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // Marks this class as a source of bean definitions for the application context
+@EnableWebSecurity	// Enables Spring Security's web security features and configuration
+public class SecurityConfig {
+    
+    // Define a SecurityFilterChain bean to configure HTTP security
+	// Once the SecurityFilterChain is defined and registered as a Spring bean, Spring Security will apply it automatically during the request-handling lifecycle.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        // Configure access control for HTTP requests
+        http
+        .authorizeHttpRequests(authz -> 
+            // Allow all requests to /api/users/** without authentication
+            authz.requestMatchers("/api/users/**").authenticated()
+        )
+        ;
+        
+        // Build and return the configured SecurityFilterChain
+        return http.build();
+    }
+}

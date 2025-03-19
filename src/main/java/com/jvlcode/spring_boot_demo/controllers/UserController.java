788a26432e59467f910202a12732970651219e55
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-03-19 11:00:49 +0530

    Revert "Update: Spring Security"
    This reverts commit caa4ac290fe6d9de2771306152111bf534d956a5.

--- src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/jvlcode/spring_boot_demo/controllers/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,9 +24,6 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
 	// ✅ GET (Get All Users)
 	@GetMapping
 	public List<UserEntity> getUsers() {
@@ -39,7 +35,6 @@ public class UserController {
 	// ✅ POST (Create New User)
     @PostMapping
     public UserEntity createUser(@RequestBody UserEntity user) {
-    	user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
     
--- src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/spring_boot_demo/entity/UserEntity.java
@@ -14,29 +14,15 @@ public class UserEntity {
 	private Long id;
 	private String name;
 	private String email;
-	private String username;
-	private String password;
 	public UserEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public UserEntity(Long id, String name, String email, String username, String password) {
+	public UserEntity(Long id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 	public Long getId() {
 		return id;
